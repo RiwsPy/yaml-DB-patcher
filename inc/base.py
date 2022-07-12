70f2d4c883dde72a6e2357d13df640774308af2e
RiwsPy <thibault.debruyne@hotmail.fr>
2022-07-12 23:20:07 +0200

    ajout soft_update

--- inc/base.py
+++ inc/base.py
@@ -7,42 +7,68 @@ from .data_dict import Data_dict
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-class YamlReader:
+class StrModel(str):
     inc_string = "£"
     path_string = "$"
 
-    def __init__(self, path="", is_first=False):
-        self.is_first = is_first
-        self._dirname = os.path.join(BASE_DIR, os.path.dirname(path))
-        self._basename = os.path.basename(path)
+    def convert_inc_string(self) -> "StrModel":
+        """
+        Permet d'unicifier un attribut en remplacant une chaîne de caractère par un identifiant sans avoir besoin de connaître sa valeur.
+        Notamment utilisé par les 'fix'.
+        """
+        splited_str = self.split(self.inc_string)
+        ret = ""
+        # ~dynamic join
+        for index, txt in enumerate(splited_str[:-1]):
+            ret = ret + txt + str(index).zfill(8)
 
+        return self.__class__(ret + splited_str[-1])
+
+    def convert_path_string(self, patchpath: str) -> "StrModel":
+        return self.__class__(self.replace(self.path_string, f"{patchpath}."))
+
+
+class YamlReader:
+    is_first = False
+    str_model = StrModel
+    db_directory = "db"
+
+    def __init__(self, path: str):
         if not os.path.exists(path):
             print(f"{path} not found.")
             raise FileNotFoundError
 
+        self._dirname = os.path.join(BASE_DIR, os.path.dirname(path))
+        self._basename = os.path.basename(path)
+
     @property
     def data(self) -> Data_dict:
-        file_content = self.read()
-        file_content = self.convert_inc_string(file_content, self.inc_string)
-        file_content = self.convert_path_to_absolute(file_content, self.path_string)
+        file_content = (
+            self.read().convert_inc_string().convert_path_string(self.patchpath)
+        )
 
         ret = Data_dict(is_first=self.is_first, **safe_load(file_content))
         ret.key_disaggregation()
         ret.extends()
+        ret.fix_me()
+        ret.resolve_links()
         return ret
 
     @property
     def abspath(self) -> str:
         """
-        Renvoie le chemin absolu du fichier
+        Renvoie le chemin absolu du fichier ou du dossier
         """
         return os.path.join(self._dirname, self._basename)
 
     @property
     def patchpath(self) -> str:
+        """
+        Renvoie le nom du fichier ou du dossier
+        """
         return os.path.basename(os.path.abspath(self._dirname))
 
-    def read(self) -> str:
+    def read(self) -> StrModel:
         read_file = ""
         if os.path.isdir(self.abspath):  # repository
             for file in os.listdir(self._dirname):
@@ -52,32 +78,19 @@ class YamlReader:
             with open(self.abspath, "r") as file_content:
                 read_file += file_content.read()
 
-        return read_file
+        return self.str_model(read_file)
 
-    @staticmethod
-    def convert_inc_string(file_content: str, inc_string: str) -> str:
-        """
-        Permet d'unicifier un attribut en remplacant une chaîne de caractère par un identifiant sans avoir besoin de connaître sa valeur.
-        Notamment utilisé par les 'fix'.
-        """
-        splited_str = file_content.split(inc_string)
-        ret = ""
-        # ~dynamic join
-        for index, txt in enumerate(splited_str[:-1]):
-            ret = ret + txt + str(index).zfill(8)
-        return ret + splited_str[-1]
+    def dump(self, filename: str = "", force: bool = False) -> None:
+        if filename == "":
+            if self._basename:
+                filename = self._basename.replace(".yaml", ".json").replace(".yml", ".json")
+            else:
+                filename = self.patchpath + ".json"
 
-    def convert_path_to_absolute(self, file_content: str, path_string: str) -> str:
-        return file_content.replace(path_string, f"{self.patchpath}.")
-
-    def dump(self, dirname="", force=False) -> None:
-        if dirname == "":
-            filename = self._basename.rpartition(".")[0] + ".json"
-            dirname = os.path.join(self._dirname, filename)
-
-        if force is False and os.path.exists(dirname):
-            print(f"Echec. {dirname} pré-existant.")
+        if force is False and os.path.exists(filename):
+            print(f"Echec. {filename} pré-existant.")
             return
 
+        dirname = os.path.join(BASE_DIR, self.db_directory, filename)
         with open(dirname, "w") as file:
             dump(self.data, file, ensure_ascii=False, indent=1)
--- inc/data_dict.py
+++ inc/data_dict.py
@@ -1,48 +1,83 @@
+import operator
+import re
 from typing import Any
+import json
+
+operator_to_method = {
+    "+": "__add__",
+    "-": "__sub__",
+    "/": "__truediv__",
+    "*": "__mul__",
+    "&": "__and__",
+    ">=": "__ge__",
+    ">": "__gt__",
+    "<=": "__le__",
+    "<": "__lt__",
+    "%": "__mod__",
+    "|": "__or__",
+    "^": "__xor__",
+    "!=": "__ne__",
+    "==": "__eq__",
+    "//": "__floordiv__",
+    "~": "__invert__",
+    "<<": "__lshift__",
+    ">>": "__rshift__",
+    "**": "__pow__",
+}
 
 
 class Data_dict(dict):
     _first_instance = None
+    regex_links_in_str = re.compile(r"(<< *([\w\. ]+) *>>)")
+    heritage_string = "<"
+    attr_split_string = "."
+    fix_string = "fix"
 
-    def __new__(cls, *args, is_first=False, **kwargs):
+    def __new__(cls, *args, is_first: bool = False, **kwargs):
         obj_id = super().__new__(cls, *args, **kwargs)
         if is_first or cls._first_instance is None:
             cls._first_instance = obj_id
         return obj_id
 
     # captation de l'attribut is_first
-    def __init__(self, *args, is_first=False, **kwargs):
+    def __init__(self, *args, is_first: bool = None, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def __getitem__(self, item: str) -> Any:
+    def __getitem__(self, key: str) -> Any:
         # suit les chemins x.y comme s'il s'agissait de {"x": {"y": object}}
-        if "." not in item:
-            return super().__getitem__(item)
+        if self.attr_split_string not in key:
+            return super().__getitem__(key)
 
         sub_dict = self
-        for key in item.split("."):
-            try:
-                sub_dict = sub_dict[key]
-            except KeyError:
-                return {}
+        for attr in key.split(self.attr_split_string):
+            sub_dict = sub_dict[attr]
         return sub_dict
 
+    def get(self, key: str, default=None) -> Any:
+        try:
+            return self.__getitem__(key)
+        except KeyError:
+            if default is None:
+                return {}
+        return default
+
     def extends(self) -> None:
+        # applique les différents héritages
         for k, v in self.items():
             if not isinstance(v, dict):
                 continue
 
-            if "<" in v:
+            if self.heritage_string in v:
                 value_expended = self.__class__()
                 for key, value in v.items():
-                    if key != "<":
+                    if key != self.heritage_string:
                         value_expended[key] = value
                         continue
 
                     # héritage multiple <: cls1 cls2 cls3
                     force_break = False
                     for entity in value.split(" "):
-                        value_expansion = self._first_instance[entity]
+                        value_expansion = self._first_instance.get(entity) or {}
                         if isinstance(value_expansion, dict):
                             value_expended.update(value_expansion)
                         else:
@@ -63,7 +98,7 @@ class Data_dict(dict):
                 dict_convert.extends()
                 self[k] = dict_convert
 
-    def key_disaggregation(self, split_string: str = ".") -> None:
+    def key_disaggregation(self) -> None:
         """
         >self.data = {'cre.PLAYER': {'name': 'player_name', 'con': 1}}
         >self.key_disaggregation()
@@ -71,9 +106,19 @@ class Data_dict(dict):
         {'cre': {'PLAYER': {'name': 'player_name', 'con': 1}}}
         """
         structured_dict = self.__class__()
+        fix_str = self.fix_string + self.attr_split_string
         for k, v in self.items():
             sub_dict = structured_dict
-            splited_keys = k.split(split_string)
+            if isinstance(k, str):
+                if k.startswith(fix_str):
+                    key_1, _, key_2 = k.partition(self.attr_split_string)
+                    if key_1 not in sub_dict:
+                        sub_dict[key_1] = dict()
+                    sub_dict[key_1][key_2] = v
+                    continue
+                splited_keys = k.split(self.attr_split_string)
+            else:
+                splited_keys = [k]
             for splited_key in splited_keys[:-1]:
                 if splited_key not in sub_dict:
                     sub_dict[splited_key] = {}
@@ -82,7 +127,83 @@ class Data_dict(dict):
             if isinstance(v, dict):
                 v = self.__class__(v)
                 v.key_disaggregation()
-            sub_dict[splited_keys[-1]] = v # dernier élément
+            sub_dict[splited_keys[-1]] = v  # dernier élément
 
         self.clear()
         self.update(structured_dict)
+
+    def fix_me(self) -> None:
+        """
+        self["OBJECT"]["fix"]["fix_id"]["fix_content"]["fix_key|method"] = fix_value
+        """
+        # TODO: rajouter des options +, -, *, /, |, &, []+...
+        for key, value in self.copy().items():
+            if not isinstance(value, dict) or self.fix_string not in value:
+                continue
+
+            # tri des fixs en fonction du pk
+            for fix_id, fix_content in sorted(
+                value[self.fix_string].items(), key=operator.itemgetter(0)
+            ):
+                if isinstance(fix_content, dict):
+                    for fix_key_with_attrs, fix_value in fix_content.items():
+                        if "|" not in fix_key_with_attrs:
+                            fix_key = fix_key_with_attrs
+                        else:
+                            splited_key = fix_key_with_attrs.split("|")
+                            fix_key = splited_key[0]
+                            methods = splited_key[1:]
+                            try:
+                                content = value[fix_key].copy()
+                            except AttributeError:
+                                content = value[fix_key]
+                            except TypeError:
+                                continue
+
+                            for method in methods:
+                                fix_value = self.apply_method(
+                                    content, fix_value, method
+                                )
+                        value[fix_key] = fix_value
+                else:
+                    # retrait de la valeur £ dans fix_id
+                    fix_id = fix_id.partition(self.attr_split_string)[2]
+                    value[fix_id] = fix_content
+
+            del value[self.fix_string]
+            value = self.__class__(value)
+            value.key_disaggregation()
+            # update_soft(self[key], value)
+            self[key] = value
+
+    def resolve_links(self) -> None:
+        def resolve_links_in_str(instance, text: str) -> str:
+            dialogs = re.split(instance.regex_links_in_str, text)
+            for index, content_id in enumerate(dialogs[2::3]):
+                new_str = resolve_links_in_str(instance, instance[content_id]) or ""
+                instance[content_id] = new_str
+                dialogs[index * 3 + 1] = ""
+                dialogs[index * 3 + 2] = new_str
+            return "".join(dialogs)
+
+        txt = resolve_links_in_str(self, json.dumps(self))
+        self.clear()
+        self.update(json.loads(txt))
+
+    @staticmethod
+    def apply_method(source: Any, value: Any, method: str) -> Any:
+        try:
+            value = value.copy()
+        except AttributeError:
+            pass
+
+        if method in operator_to_method:
+            try:
+                return getattr(source, operator_to_method[method])(value)
+            except AttributeError:
+                print(
+                    f"fix non appliqué: {type(source)} have no method for operator {method}"
+                )
+            except TypeError:
+                print(f"fix non appliqué: type incorrect {type(source)}, {type(value)}")
+        return value
